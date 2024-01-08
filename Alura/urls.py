1ebedcf269f47ef9d4c7ec5fe78c8ecf5eb6505b
Paulo fernando de oliveira <paulof.oliveira00@gmail.com>
2024-01-08 14:50:48 -0300

    pausei aqui, pois fui estudar html, ja estamos exibindo o formulario de cadastro, falta apenas colocar as funcoes

--- Alura/urls.py
+++ Alura/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from app_video import views
+
+
 urlpatterns = [
     # Exemplos:
     # path('', Alura.views.home, name='home'),
     # path('blog/', include('blog.urls')),
     path('videos', views.home, name='home'),
+    path('videos/<int:video_id>/', views.video_especifico, name='video_especifico'),
+    path('cadastrar', views.cadastrar_videos, name='cadastro_de_videos')
 ]
--- app_video/views.py
+++ app_video/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Video
 
 def home(request):
     videos = Video.objects.all()
     return render(request, 'todos_os_videos/todos_os_videos.html', {'videos': videos})
+
+
+
+def video_especifico(request, video_id):
+    video = get_object_or_404(Video, pk=video_id)
+    return render(request, 'todos_os_videos/videos_unico.html', {'video': video})
+
+def cadastrar_videos(request):
+    return render (request, 'todos_os_videos/cadastrar_videos.html')
